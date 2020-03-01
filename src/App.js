46c58604db5eef668815003790626ff91777437e
celinechole <github@celinechole.com>
2020-03-01 10:29:54 -0500

    call api all pokemon

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {
   getAllPokemon
 } from './Store';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <button onClick={async () => getAllPokemon()}>Get Pokemon</button>
-    </div>
+   <div><button onClick={async () => alert(await getAllPokemon())}>get pokemon</button></div>
   );
 }
 
--- src/Store/index.js
+++ src/Store/index.js
@@ -1,5 +1,11 @@
 const getAllPokemon = async () => {
-  alert('Get All Pokemon');
-}
+  const result = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=5");
+  const resultJson = await result.json();
+  const allPokemon = resultJson.results;
+  const pokemon = allPokemon.map(pokemon => {
+    return { name: pokemon.name };
+  });
+  return pokemon;
+};
 
 export { getAllPokemon };
