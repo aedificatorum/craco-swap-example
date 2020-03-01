5738e99a47754eb9ed5a883d1634fbbb94e5a30d
Tim Addison <git@tjaddison.com>
2020-03-01 16:35:05 -0500

    aria-label for emojis

--- src/PokemonDetails.js
+++ src/PokemonDetails.js
@@ -5,8 +5,8 @@ const PokemonDetails = ({ pokemon }) => {
     <div>
       <section className="pokemon-card">
         <div className="name">{pokemon.name}</div>
-        <div>📏: {pokemon.height}</div>
-        <div>⚖: {pokemon.weight}</div>
+        <div><span role="img" aria-label="height">📏</span>: {pokemon.height}</div>
+        <div><span role="img" aria-label="weight">⚖</span>: {pokemon.weight}</div>
         <div>{pokemon.types}</div>
       </section>
     </div>
