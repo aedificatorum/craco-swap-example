47b585f18f94867d2bee50c9fc17826782604eb8
celinechole <github@celinechole.com>
2020-03-01 11:47:46 -0500

    update pokemon card

--- src/PokemonDetails.js
+++ src/PokemonDetails.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
-const PokemonDetails = ({ props }) => {
-  console.log(props)
+const PokemonDetails = ({ pokemon }) => {
   return (
     <div>
-      Test
+      {pokemon.name}
     </div>
   )
 }
