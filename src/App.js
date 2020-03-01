e7369619bd29781ca58d536c4b1e20cb6e412f8f
Tim Addison <git@tjaddison.com>
2020-03-01 10:41:32 -0500

    show pokemon list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import './App.css';
-import {
-  getAllPokemon
-} from './Store';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import PokemonList from "./PokemonList";
+import { getAllPokemon } from "./Store";
+
+const Loading = () => {
+  return <div>Loading...</div>;
+};
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    const getPokemonAsync = async () => {
+      setPokemonList(await getAllPokemon());
+    };
+    getPokemonAsync();
+  }, []);
 
   return (
-   <div><button onClick={async () => alert(await getAllPokemon())}>get pokemon</button></div>
+    <div>
+      <h1>Pokebattle</h1>
+      <div>
+        {!pokemonList.length ? (
+          <Loading />
+        ) : (
+          <PokemonList pokemon={pokemonList} />
+        )}
+      </div>
+    </div>
   );
 }
 
